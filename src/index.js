fa2cad9054713db7ca0fa48b311a37480a54d279
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2017-04-07 06:49:12 +0100

    Add initial redux structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+
 import App from './App';
 import './index.css';
 
+import rootReducer from './reducers';
+
+const middleware = [];
+if (process.env.NODE_ENV === 'production') {
+  const createLogger = require('redux-logger').createLogger;
+  middleware.push(createLogger());
+}
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middleware)
+);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
